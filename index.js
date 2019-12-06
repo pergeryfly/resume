a15d2f6797ef7d4a6bc7a116967f675a62e99201
pergeryfly <pergeryfly@gmail.com>
2019-12-06 14:29:33 +0800

    js文件使用es6引用

---
+++ index.js
@@ -0,0 +1,7 @@
+import "./css/style.css"
+// import swiper from 'swiper'
+// import leancloudStorage from 'leancloud-storage'
+// import './node_modules/swiper/js/swiper.min.js'
+// import './node_modules/swiper/css/swiper.min.css'
+import "./js/main.js"
+import "./js/message.js"
